3d0deca3cb17a5cfce740d3aeb207d5549660e2b
artem <artem140742@gmail.com>
2023-04-10 22:46:15 +0300

    random character add + loaded character by ID

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
+// import MarvelService from "./services/MarvelService";
 import "./style/style.scss";
 
-const marvelService = new MarvelService();
+// const marvelService = new MarvelService();
 
-marvelService.getAllCharacters().then((res) =>
-  res.data.results.forEach((element) => {
-    console.log(element.name);
-  })
-); // отрімуємо усіх персонажів
-// marvelService.getCharacterId(1011334).then((res) => console.log(res)); //отримуємо одного персонажа по id
+// marvelService.getAllCharacters().then((res) =>
+// 	res.data.results.forEach((element) => {
+// 		console.log(element.name);
+// 	}),
+// ); // отрімуємо усіх персонажів
+// marvelService
+// 	.getCharacterById(1011005)
+// 	.then((res) => console.log(res.data.results[0].name)); //отримуємо імя одного персонажа по id
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
---
+++ src/services/MarvelService.jsx
@@ -0,0 +1,29 @@
+class MarvelService {
+	_apiBase = "https://gateway.marvel.com:443/v1/public/";
+	_apiKey = "apikey=f0f78042985ef6470c7dc2ef4d8b5380";
+
+	getResource = async (url) => {
+		let res = await fetch(url);
+
+		if (!res.ok) {
+			new Error(
+				`Could not fetch ${url}, check the status: ${res.status}`,
+			);
+		}
+		return await res.json();
+	};
+
+	//делаєм запроси до api
+
+	//отримуєм усіх персонажів
+	getAllCharacters = () => {
+		return this.getResource(`${this._apiBase}characters?limit=9&offset=100&${this._apiKey}`); //берем данні з акк арі
+	};
+
+	getCharacterById = (id) => {
+		// отримуємо одного персонжа по id
+		return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+	};
+}
+
+export default MarvelService;
