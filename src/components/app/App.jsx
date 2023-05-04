d2803c6df67ee798d7434ac75815cc1c07ea0096
artem <artem140742@gmail.com>
2023-05-04 16:28:53 +0300

    lazy components(pages) loading

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,16 @@
+import { lazy, Suspense } from "react";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { MainPage, ComicsPage, SingleComicPage, Page404 } from "../pages";
+//import { MainPage, ComicsPage, SingleComicPage } from "../pages";
+import Spinner from "../spinner/Spinner";
 import AppHeader from "../appHeader/AppHeader";
 
+//линіва підгрузка сторінок(компонентів) 
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 const App = () => {
     return (
@@ -10,20 +18,22 @@ const App = () => {
             <div className="app">
                 <AppHeader />
                 <main>
-                    <Switch>
-                        <Route exact path={'/'}> {/*'/' так атребут слеш указує на основну сторінку, exact - треба ставити завжди щоб правильно грузилась сторінка */}
-                            <MainPage />
-                        </Route>
-                        <Route exact path={'/comics'}>
-                            <ComicsPage />
-                        </Route>
-                        <Route exact path={'/comics/:comicId'}>  {/* будь-як можна називати ID */}
-                            <SingleComicPage />
-                        </Route>
-                        <Route path='*'>
-                            <Page404 />
-                        </Route>
-                    </Switch>
+                    <Suspense fallback={<Spinner />}>
+                        <Switch>
+                            <Route exact path={'/'}> {/*'/' так атребут слеш указує на основну сторінку, exact - треба ставити завжди щоб правильно грузилась сторінка */}
+                                <MainPage />
+                            </Route>
+                            <Route exact path={'/comics'}>
+                                <ComicsPage />
+                            </Route>
+                            <Route exact path={'/comics/:comicId'}>  {/* будь-як можна називати ID */}
+                                <SingleComicPage />
+                            </Route>
+                            <Route path='*'>
+                                <Page404 />
+                            </Route>
+                        </Switch>
+                    </Suspense>
                 </main>
             </div>
         </Router >
